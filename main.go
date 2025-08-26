2411ee3ab5364ae4aee2327f790feb5423a57d05
Carlos Camilo <carloscamilocontato@gmail.com>
2025-08-26 19:16:16 -0300

    ch 1

--- main.go
+++ main.go
@@ -1,7 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+)
+
+func getLinesChannel(file io.ReadCloser) <-chan string {
+	ch := make(chan string)
+
+	go func() {
+		currentLine := ""
+		for {
+			content := make([]byte, 8)
+			_, err := file.Read(content)
+			if err != nil && err != io.EOF {
+				panic(err)
+			}
+
+			if err == io.EOF {
+				close(ch)
+				break
+			}
+
+			if readAt := bytes.IndexByte(content, '\n'); readAt != -1 {
+				currentLine += string(content[:readAt])
+				ch <- currentLine
+
+				content = content[readAt+1:]
+				currentLine = ""
+			}
+
+			currentLine += string(content)
+		}
+	}()
+
+	return ch
+}
 
 func main() {
-	fmt.Println("I hope I get the job!")
+	file, err := os.Open("./messages.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	contentsCh := getLinesChannel(file)
+	for line := range contentsCh {
+		fmt.Printf("read: %s\n", line)
+	}
 }
